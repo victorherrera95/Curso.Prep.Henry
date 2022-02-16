2257c523511cabb4720dc82aadd480227795d03d
victorherrera95 <herreracontrerasva@gmail.com>
2022-02-16 16:44:47 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -73,7 +73,7 @@ function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
   //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
-  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
+  //Si num3 es más grande que num3 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
 }
